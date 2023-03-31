65897c41696e8b9ddc1de0dbf00dc84e30528489
jahangir2k04 <jahangir2k04gmail.com>
2023-03-31 20:18:51 +0600

    blogs data loaded and set it to setBlogs

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = () => {
+    return (
+        <div>
+            <h4>Here is the first blog</h4>
+        </div>
+    );
+};
+
+export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Blogs.css'
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect( ()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+
+
     return (
         <div className='blogs-container'>
 
             <div className='blogs'>
-                <h1>this is blogs container</h1>
+                <Blog></Blog>
             </div>
             <div className='bookmarks-blogs'>
                 <h1>this is bookmarks container</h1>
             </div>
-            
+
         </div>
     );
 };
