aa84355bebc8319d301332a907e14b95812ec743
jahangir2k04 <jahangir2k04gmail.com>
2023-03-31 20:00:35 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h3>Welcome react first assignment</h3>
+      <Header></Header>
+      <hr />
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h3>Programming Hub</h3>
+            <div className='header-item'>
+                <a href="/home">Home</a>
+                <a href="/about">About us</a>
+                <a href="/contact">Contact</a>
+                <a href="/Login">Login</a>
+                <img src="https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?b=1&s=170667a&w=0&k=20&c=ZAXJYLesh6gSd9huAgpy6rjpR4z-IFVH9MpxrKIXCrs=" alt="" />
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
