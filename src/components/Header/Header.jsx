ac31392d893efaded165e31041f6f1d967211098
jahangir2k04 <jahangir2k04@gmail.com>
2023-08-25 21:22:10 +0600

    route issue solved

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
             <h3>Programming Hub</h3>
             <div className='header-item'>
                 <a href="/home">Home</a>
-                <a href="/contact">Contact</a>
-                <a href="/Login">Login</a>
+                <a href="/">Contact</a>
+                <a href="/">Login</a>
                 <img src="https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?b=1&s=170667a&w=0&k=20&c=ZAXJYLesh6gSd9huAgpy6rjpR4z-IFVH9MpxrKIXCrs=" alt="" />
             </div>
         </nav>
